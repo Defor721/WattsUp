59a431fbee08a5016b610375e3744a59e78d1af7
Defor721 <92020095+Defor721@users.noreply.github.com>
2024-12-03 14:44:52 +0900

    [Feat] mongodb 연결 #1

---
+++ src/app/trade/page.tsx
@@ -0,0 +1,13 @@
+function page() {
+  return (
+    <div>
+      <div>트레이드임</div>
+      <div>
+        <div>뭐어케하지?</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+    </div>
+  );
+}
+export default page;
