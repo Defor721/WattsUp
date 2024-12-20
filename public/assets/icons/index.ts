68f6c3947f2e5eab7d36023f0f89e67d43edc4c2
Yoonstar <yagobo1110@naver.com>
2024-12-20 14:29:28 +0900

    [Feat] 화면 오른쪽 하단 플로팅 버튼 생성, 화면 최상단 이동 함수 생성 #112

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -1,12 +1,11 @@
 import { IoMdHome } from "react-icons/io";
-import { FaExchangeAlt } from "react-icons/fa";
+import { FaExchangeAlt, FaChevronRight } from "react-icons/fa";
 import { IoMdSettings } from "react-icons/io";
-import { FaChevronRight } from "react-icons/fa";
-import { IoLogInOutline } from "react-icons/io5";
-import { IoPersonOutline } from "react-icons/io5";
-import { FaChartLine } from "react-icons/fa";
-import { FaDollarSign } from "react-icons/fa";
+import { IoLogInOutline, IoPersonOutline } from "react-icons/io5";
+import { FaChartLine, FaDollarSign, FaArrowUp } from "react-icons/fa";
 import { Eye, EyeOff } from "lucide-react";
+import { HiOutlineDotsVertical } from "react-icons/hi";
+import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
 
 export {
   IoMdHome,
@@ -19,4 +18,8 @@ export {
   FaDollarSign,
   Eye,
   EyeOff,
+  HiOutlineDotsVertical,
+  FaArrowUp,
+  MdOutlineLightMode,
+  MdOutlineDarkMode,
 };
