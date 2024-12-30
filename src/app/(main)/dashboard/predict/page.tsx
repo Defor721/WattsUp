7e4084476c26695b804320db7817169c03c75d72
LifeIsMoment <yun777567@gmail.com>
2024-12-30 20:07:24 +0900

    [Feat] 대시보드 오류 해결완료, 추가 대시보드 완성 #88

--- src/app/(main)/dashboard/predict/page.tsx
+++ src/app/(main)/dashboard/predict/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 import PredictMain from "@/components/dashboard/predict/Main";
+import Loading from "@/app/loading";
 
 function Page() {
-  return <PredictMain />;
+  // return <PredictMain />;
+  return <Loading />;
 }
 
 export default Page;
