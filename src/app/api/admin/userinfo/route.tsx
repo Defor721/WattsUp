bc96804ccb98b5dde1f635ddb1a1d42ab4784176
Defor721 <92020095+Defor721@users.noreply.github.com>
2024-12-31 14:26:45 +0900

    [Feat] new api #21

---
+++ src/app/api/admin/userinfo/route.tsx
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+import clientPromise from "@/lib/mongodb";
+
+export async function GET(request: NextRequest) {
+  try {
+    const client = await clientPromise;
+    const db = client.db("wattsup");
+    const collection = db.collection("userdata");
+    const users = await collection
+      .find({})
+      .project({ _id: 0, password: 0 })
+      .toArray();
+    return NextResponse.json({ message: "success", users }, { status: 200 });
+  } catch (error: unknown) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error";
+    return NextResponse.json(
+      { message: "Failed to find data", error: errorMessage },
+      { status: 500 },
+    );
+  }
+}
