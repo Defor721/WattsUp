1adad51099aeadcde8e4458cb9c7057b51a58d2b
hayoung <duddldml123@naver.com>
2024-12-31 16:34:11 +0900

    [Fea] ui 대공사 #106

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import $ from "jquery";
 
 import "fullpage.js/dist/jquery.fullpage.css";
-import "fullpage.js"; // fullpage.js 로드
+import "fullpage.js";
 import VideoPart from "@/components/main/video/VideoPart";
 import EnergyInfoPage from "@/components/main/renewable/renewable";
 import FeaturesPart from "@/components/main/features/Features";
@@ -12,24 +12,33 @@ import FeaturesPart from "@/components/main/features/Features";
 export default function Home() {
   useEffect(() => {
     if (typeof window !== "undefined") {
-      // fullpage.js 초기화
-      $("#fullpage").fullpage({
-        navigation: true, // 오른쪽 네비게이션 활성화
-        scrollingSpeed: 700, // 스크롤 속도
-        anchors: ["video", "energy", "features"], // 앵커 설정
+      $(document).ready(() => {
+        // fullpage.js 초기화
+        $("#fullpage").fullpage({
+          navigation: true,
+          scrollingSpeed: 700,
+          anchors: ["video", "energy", "features"],
+        });
       });
     }
+
+    return () => {
+      if ($.fn.fullpage.destroy) {
+        // 컴포넌트 언마운트 시 fullpage.js 제거
+        $.fn.fullpage.destroy("all");
+      }
+    };
   }, []);
 
   return (
     <div id="fullpage">
-      <div className="section">
+      <div className="section" style={{ backgroundColor: "#e5e7eb" }}>
         <VideoPart />
       </div>
-      <div className="section">
+      <div className="section" style={{ backgroundColor: "#f3f4f6" }}>
         <EnergyInfoPage />
       </div>
-      <div className="section">
+      <div className="section" style={{ backgroundColor: "#ffffff" }}>
         <FeaturesPart />
       </div>
     </div>
