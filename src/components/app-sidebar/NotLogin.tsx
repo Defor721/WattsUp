596e16230b0eaf12c7377f438dc590c4d50cd120
Yoonstar <yagobo1110@naver.com>
2025-01-06 16:57:18 +0900

    [Feat] develop 브랜치 병합 후 확인 #118

--- src/components/app-sidebar/NotLogin.tsx
+++ src/components/app-sidebar/NotLogin.tsx
@@ -11,7 +11,7 @@ function NotLogin({ isTablet, isTabletExpanded }: Props) {
   if (isTablet && !isTabletExpanded)
     return (
       <Link
-        className="flex items-center gap-3 rounded-lg px-3 py-2 text-sm font-medium transition-all duration-200 hover:scale-105 hover:bg-white hover:text-[rgb(7,15,38)] hover:opacity-80"
+        className="flex items-center gap-3 rounded-lg px-3 py-2 text-sm font-medium transition-all duration-200 hover:bg-[rgb(20,35,80)] hover:text-white"
         href={"/login"}
       >
         <LogIn />
@@ -21,7 +21,7 @@ function NotLogin({ isTablet, isTabletExpanded }: Props) {
   // 테블릿 화면에서 사이드바 확장 and 평상시 화면
   return (
     <Link
-      className="flex items-center gap-3 rounded-lg px-3 py-2 text-sm font-medium transition-all duration-200 hover:scale-105 hover:bg-white hover:text-[rgb(7,15,38)] hover:opacity-80 md:text-base"
+      className="flex items-center gap-3 rounded-lg px-3 py-2 text-sm font-medium transition-all duration-200 hover:bg-[rgb(20,35,80)] hover:text-white md:text-base"
       href={"/login"}
     >
       <LogIn />
