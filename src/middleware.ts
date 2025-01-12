a2ad4ebeca811daca348a2ab61c29d9a747f19f2
Defor721 <92020095+Defor721@users.noreply.github.com>
2025-01-12 19:47:07 +0900

    [Feat] new api #21

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,13 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("accessToken")?.value;
 
-  if (!token) return NextResponse.redirect(new URL("/login", req.url));
+  console.log("middleware AccessToken:", token); // TODO: 디버깅용 로그
+  console.log("middleware Request URL:", req.url); // TODO: 디버깅용 로그
+
+  if (!token)
+    return NextResponse.redirect(new URL("/login", req.url), {
+      headers: { "Cache-Control": "no-store" },
+    });
 
   return NextResponse.next();
 }
