ef435b94d11367844b9939bbe3d3837b714ab20c
hayoung <duddldml123@naver.com>
2025-02-06 14:43:31 +0900

    [feat] 잠시 수정 #248

--- src/components/introduce/KakaoMap.tsx
+++ src/components/introduce/KakaoMap.tsx
@@ -42,6 +42,7 @@ const KakaoMap = ({ onRegionClick }: KakaoMapProps) => {
     const MAP_KEY = process.env.NEXT_PUBLIC_KAKAO_API_KEY;
 
     script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${MAP_KEY}&autoload=false`;
+
     script.async = true;
 
     script.onload = () => {
