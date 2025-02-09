0c1e16a01f60adb245d98bcd794cb9924829f327
working-zima <working.zima@gmail.com>
2025-02-09 16:13:06 +0900

    [Refactor] 카카오맵 환경변수 관련 버그 수정 #259

--- src/components/introduce/KakaoMap.tsx
+++ src/components/introduce/KakaoMap.tsx
@@ -39,9 +39,7 @@ const KakaoMap = ({ onRegionClick }: KakaoMapProps) => {
   // 카카오 맵 초기화 및 마커 설정
   useEffect(() => {
     const script = document.createElement("script");
-    const MAP_KEY = process.env.NEXT_PUBLIC_KAKAO_API_KEY;
-
-    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${MAP_KEY}&autoload=false`;
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&autoload=false`;
     script.async = true;
 
     script.onload = () => {
